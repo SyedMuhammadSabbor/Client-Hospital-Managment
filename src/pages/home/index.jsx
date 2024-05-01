13c70c6101577f290943e19006fc2cd40636f643
sabborshah0501@gmail.com <sabborshah0501@gmail.com>
2024-05-01 16:33:15 +0500

    login page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      <p className='text-red-500'>thats our home page</p>
+      <p className='text-red-500'>Welcome to Home Page</p>
     </div>
   )
 }
